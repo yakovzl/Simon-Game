048777c4cbe47db84eedaad73ba3eae60820bf73
yakovzl <heimzl1996@gmail.com>
2021-06-09 09:21:20 +0300

    addad all fiels into stage area for uploding to git

---
+++ game.js
@@ -0,0 +1,100 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level = 0;
+var started = false;
+
+$(document).keydown(function(event) {
+
+  if (started === false) {
+    nextSequence();
+    started = true;
+  }
+
+});
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+
+
+    gameOver();
+  }
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  playSound(randomChosenColour);
+  level++;
+  $("#level-title").text("Level " + level);
+
+}
+
+
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+
+};
+
+
+
+function gameOver() {
+  console.log("game");
+  playSound("wrong");
+  $("#level-title").text("Game Over, Pressed Eny Key To Restart");
+  $("body").addClass("game-over");
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+  }, 1000);
+
+  level = 0;
+  gamePattern = [];
+  started = false;
+
+
+
+};
